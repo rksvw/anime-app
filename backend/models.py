9cbb3f84d60191900b20d202e2c070bcb7b6fdf5
rksvw <ritik.vw98@gmail.com>
2024-02-20 20:23:26 +0530

    Message check

---
+++ backend/models.py
@@ -0,0 +1,15 @@
+from config import db
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "email": self.email,
+        }
